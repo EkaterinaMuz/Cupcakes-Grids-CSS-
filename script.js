8a6d9ef89d135bc6380308f27b1a827a793b2d57
Ekaterina Muzheva <ekaterina.muzheva11@gmail.com>
2023-03-01 19:36:47 +0800

    ready

---
+++ script.js
@@ -0,0 +1,56 @@
+const percentage = document.querySelectorAll('.skill__percentage'),
+    line = document.querySelectorAll('.skill__line span');
+
+    percentage.forEach((item, i) => {
+        line[i].style.width = item.textContent
+        console.log(item.innerHTML);
+    }); 
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu')
+    closeTrigger = document.querySelector('.menu__close');
+    overlay = document.querySelector('.menu__overlay'),
+    menuItem = document.querySelectorAll('.menu__item');
+
+hamburger.addEventListener('click', openMenu);
+closeTrigger.addEventListener('click', closeMenu);
+overlay.addEventListener('click', closeMenu);
+window.addEventListener('keydown', (e) => {
+    if(e.code === 'Escape') {
+        closeMenu();
+    }
+})
+
+for(item of menuItem) {
+    item.addEventListener('click', closeMenu);
+}
+
+function openMenu() {
+    menu.classList.add('menu_active');
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+};
+
+function closeMenu() {
+    menu.classList.remove('menu_active')
+    overlay.style.display = 'none';
+    document.body.style.overflow = '';
+};
+
+
+
+const up = document.querySelector('#up');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > 1300) {
+        up.style.display = 'block'
+    } else {
+        up.style.display = 'none'
+    }
+});
+
+up.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+    // document.documentElement.scrollTop = 0;
+})
+
